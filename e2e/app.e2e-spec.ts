2a2ad0e481325c99c7ff330e98605acfed489f9e
Leonardo Pignataro <leopignataro@gmail.com>
2018-03-19 00:34:32 -0300

    Projeto convertido para projeto Angular

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('metadoc-site App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
