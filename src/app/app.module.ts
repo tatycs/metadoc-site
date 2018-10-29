a7cd9e0a93e4f1a0de64d15c58a34cab46f010c5
Leonardo Pignataro <leopignataro@gmail.com>
2018-10-28 21:05:17 -0300

    Added routing; created "course" page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CourseComponent } from './components/course/course.component';
 
-import { AppComponent } from './app.component';
-
+const appRoutes: Routes = [
+	//{ path: 'home', component: HomeComponent },
+	{ path: 'course', component: CourseComponent },
+	{
+		path: '',
+		//redirectTo: '/home',
+		component: HomeComponent,
+		pathMatch: 'full'
+	}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		CourseComponent
+	],
+	imports: [
+		RouterModule.forRoot(appRoutes),
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
